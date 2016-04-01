e731fc01f96c3a50cba9fb8843195bdb55a75e8e
Jay Shenk <jay.shenk@gmail.com>
2016-04-01 18:20:05 -0400

    Complete nth prime challenge

---
+++ nth_prime/nth_prime.rb
@@ -0,0 +1,22 @@
+class Prime
+  def self.nth(number)
+    fail ArgumentError, 'Number must be greater than 0' if number <= 0
+
+    primes = [2]
+
+    current_num = 3
+    while primes.size < number
+      stop_num = Math.sqrt(current_num)
+      primes.each do |prime|
+        break if current_num % prime == 0
+        if prime > stop_num
+          primes << current_num
+          break
+        end
+      end
+      current_num += 1
+    end
+
+    primes.last
+  end
+end
